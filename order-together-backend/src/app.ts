6893effb643bd9556153b4403269020415ab6114
DaisyMA666 <maqing2368@gmail.com>
2024-06-09 17:45:02 -0700

    set up database

--- order-together-backend/src/app.ts
+++ order-together-backend/src/app.ts
@@ -1,15 +1,18 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import {Application} from "express";
+import 'reflect-metadata';
+import express from 'express';
+import * as bodyParser from 'body-parser';
+import { Application } from 'express';
+import gDB from './initDataSource';
+import dotenv from 'dotenv-flow';
+
+dotenv.config();
+
 
 const startServer = async () => {
-    try{
-        await createConnection()
+    try {
+        await gDB.initialize();
 
-
-        const app: Application = express()
+        const app: Application = express();
 
         app.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -18,18 +21,14 @@ const startServer = async () => {
             next();
         });
 
-
-        app.use(bodyParser.json())
-
-
+        app.use(bodyParser.json());
 
         app.listen(process.env.PORT, () => {
-            console.log(`Express server has started on port ${process.env.PORT}`)
-        })
-
-    }catch (e){
-        console.log(e)
+            console.log(`Express server has started on port ${process.env.PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
     }
-}
+};
 
-startServer()
+startServer();
