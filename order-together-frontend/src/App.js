74e581bff45e39f9c4d1fb2bd904f697de60e64e
Sijiang <dandingirl43@gmail.com>
2024-06-16 22:27:02 -0700

    update test

--- order-together-frontend/src/App.js
+++ order-together-frontend/src/App.js
@@ -15,6 +15,7 @@ function App () {
           <Route path="login" element={<LogIn/>}/>
           {/*<Route path="signup" element={<SignUp/>} />*/}
         {/*</Route>*/}
+          {/*sijiang test*/}
       </Routes>
     </BrowserRouter>
   )
