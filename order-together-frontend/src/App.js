23bb3864b5214d3ac5c9f259588675bf20b8f213
harry8hay <harrymzf@gmail.com>
2024-06-25 18:02:48 -0700

    homepagebackend1.01

--- order-together-frontend/src/App.js
+++ order-together-frontend/src/App.js
@@ -3,28 +3,6 @@ import './App.css'
 import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms'
 import Button from '@mui/material/Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React<AccessAlarmsIcon/> //Harry is here1. Qing is here3. Ethan-test is here3. Sijiang is here1.
-          <Button variant="contained">Contained</Button>
-        </a>
-      </header>
-    </div>
-  );
-}
-
 function App () {
   return (
     <BrowserRouter>
