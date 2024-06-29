f636bf9f04aeb07f01699928c1203d8a708fa725
SijiangZhou <dandingirl43@gmail.com>
2024-06-29 15:09:25 -0700

    update homepage

--- order-together-frontend/src/component/login/SignUp.js
+++ order-together-frontend/src/component/login/SignUp.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import * as yup from 'yup'
 import Button from '@mui/material/Button'
@@ -76,9 +77,9 @@ export const SignUp = () => {
         message: existRes.message
       })
       if (existRes.error || existRes.existed) {
-          updateValidationResults(name, existRes.message)
+        updateValidationResults(name, existRes.message)
       } else {
-          updateValidationResults(name)
+        updateValidationResults(name)
       }
     }
   }
@@ -92,12 +93,12 @@ export const SignUp = () => {
         const existRes = await dispatch(usernameExistedCheck(formData.username))
         setIsCheckingBackEnd(false)
         if (existRes.error || existRes.existed) {
-            setValidationRes(prevState => ({
-                ...prevState,
-                isValid: false,
-                errs: {...prevState.errs, username: existRes.message}
-            }))
-            return
+          setValidationRes(prevState => ({
+            ...prevState,
+            isValid: false,
+            errs: {...prevState.errs, username: existRes.message}
+          }))
+          return
         }
       }
 
@@ -130,13 +131,13 @@ export const SignUp = () => {
   return (
     <Box
       sx={{
-        height: { xs: '934px', md: '706px' },
+        height: 'auto',
         display: 'flex',
         flexDirection: 'column',
         position: 'relative',
         paddingTop: 0,
         paddingBottom: 0,
-        background: 'rgba(240, 240, 240, 1)'
+        background: 'rgba(240, 240, 240, 1)',
       }}
     >
       <Box
@@ -161,7 +162,7 @@ export const SignUp = () => {
             paddingLeft: '16px !important',
             paddingRight: '16px !important',
             width: { md: '466px' },
-            height: { xs: '932px', md: '520px' }
+            height: { xs: '932px', md: '380px' }
           }}
         >
           <Box
@@ -358,7 +359,7 @@ export const SignUp = () => {
                 {isCheckingBackEnd ? <CircularWaiting size={20}/> : 'Sign Up'}
               </Button>
             </Box>
-            <Box sx={{ display: 'flex', mt: '24px', fontFamily: '\'Inter\', sans-serif' }}>
+            <Box sx={{ display: 'flex', mt: '-40px', fontFamily: '\'Inter\', sans-serif' }}>
               <Typography>Already have an account?</Typography>
               <Typography sx={{ ml: '8px' }}>
                 <Link
---
+++ order-together-frontend/src/component/public/Footer.js
@@ -0,0 +1,21 @@
+import '../main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+export const Footer = () => {
+
+  return (
+    <div id="footer" className="footer">
+      <div class="social-media">
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fab fa-linkedin"></i>
+      </div>
+      <div class="footer-nav">
+        <a href="#">Terms</a>
+        <a href="#">Privacy</a>
+        <a href="#">My Policy</a>
+      </div>
+    </div>
+  )
+}
