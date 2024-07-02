6d8427e024479ac02d0350526a6494e7c703226e
DaisyMA666 <maqing2368@gmail.com>
2024-07-02 15:58:44 -0700

    Update App.js

--- order-together-frontend/src/App.js
+++ order-together-frontend/src/App.js
@@ -7,27 +7,12 @@ import { MainLayout } from './component/public/MainLayout.js'
 import { LogIn } from './component/login/LogIn.js'
 import { SignUp } from './component/login/SignUp.js'
 import { Initiate } from './component/Initiate.js'
-<<<<<<< HEAD
-=======
 import { Homepage } from './component/homepage/Homepage.js'
 import { JointOrder } from './component/jointOrder/JointOrder.js'
->>>>>>> f831ed679273405bb7951966510ee921ef4bf035
 
 function App () {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <Routes>
-        {/*<Route path="/" element={<MainLayout/>}>*/}
-        {/*<Route path="/" element={<LogIn/>}></Route>*/}
-          <Route path="login" element={<LogIn/>}/>
-          {/*<Route path="signup" element={<SignUp/>} />*/}
-        {/*</Route>*/}
-          {/*sijiang test*/}
-          <Route path="signup" element={<SignUp/>}/>
-          <Route path="initiate" element={<Initiate/>}/>
-      </Routes>
-=======
       <MainLayout>
         <Routes>
           <Route path="/" element={<Homepage/>}/>
@@ -37,7 +22,6 @@ function App () {
           <Route path="joint/:uid" element={<JointOrder/>}/>
         </Routes>
       </MainLayout>
->>>>>>> f831ed679273405bb7951966510ee921ef4bf035
     </BrowserRouter>
   )
 }
