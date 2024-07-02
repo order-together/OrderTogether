b376019e643a263bd250e6abf34f5839cb243b25
DaisyMA666 <maqing2368@gmail.com>
2024-07-02 16:06:20 -0700

    Update MainLayout.js

--- order-together-frontend/src/component/public/MainLayout.js
+++ order-together-frontend/src/component/public/MainLayout.js
@@ -1,18 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-
-
-export const MainLayout = ({ children }) => {
-  // const dispatch = useDispatch()
-  // const location = useLocation()
-  // const currentRoute = location.pathname
-=======
 import { Header } from './Header.js'
 import { Footer } from './Footer.js'
 
 
 export const MainLayout = ({ children }) => {
->>>>>>> f831ed679273405bb7951966510ee921ef4bf035
 
   return (
     <div
@@ -22,22 +13,6 @@ export const MainLayout = ({ children }) => {
         minHeight: '100vh'
       }}
     >
-<<<<<<< HEAD
-      {/*<Header/>*/}
-      {/*<div*/}
-      {/*  style={{*/}
-      {/*    flex: 1,*/}
-      {/*    overflowY: 'auto',*/}
-      {/*    margin: 0*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*  {children}*/}
-      {/*</div>*/}
-      {/*{currentRoute !== '/login' && currentRoute !== '/signup' && <Footer/>}*/}
-    </div>
-  )
-}
-=======
       <Header/>
       <div
         style={{
@@ -52,4 +27,3 @@ export const MainLayout = ({ children }) => {
     </div>
   )
 }
->>>>>>> f831ed679273405bb7951966510ee921ef4bf035
