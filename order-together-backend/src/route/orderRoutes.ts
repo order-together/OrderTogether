3e24da66a4326dfec33977e937fd5c2b8b2994b1
DAISY MA <maqing2368@gmail.com>
2024-07-03 17:19:10 -0700

    update manage order

--- order-together-backend/src/route/orderRoutes.ts
+++ order-together-backend/src/route/orderRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { getOrders, addOrder } from '../controller/orderController';
+// import {getOrders, addOrder, orderController} from '../controller/orderController';
+import {orderController} from "../controller/orderController";
 
 const router = Router();
 
-router.get('/orders', getOrders);
-router.post('/orders', addOrder);
+// router.get('/orders', getOrders);
+// router.post('/orders', addOrder);
 
+
+router.get('/getInitiatorOrders', orderController.getInitiatorOrders);
 export default router;
--- order-together-frontend/src/component/public/Header.js
+++ order-together-frontend/src/component/public/Header.js
@@ -1,15 +1,14 @@
 
 import '../main.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-
 export const Header = () => {
 
   return (
     <div id="header" className="header">
-      <img className="logo" src="./logo.png" alt=""/>
+      <img className="logo" src="logo.png" alt=""/>
       <nav>
         <a href="http://localhost:3000/">PRODUCT</a>
-        <a href="#">ORDERS</a>
+        <a href="http://localhost:3000/manage">ORDERS</a>
         <a href="http://localhost:3000/initiate">CREATE</a>
       </nav>
       <div className="right-header">
